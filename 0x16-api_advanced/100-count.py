6a8cdcd6373ba055236be984cc8908081b4624a5
NaomiKleinhans <naomikleinhans56@gmail.com>
2023-08-09 23:40:11 +0200

    Fixes

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-import requests
+"""Module for task 3"""
+
+
 def count_words(subreddit, word_list, word_count={}, after=None):
     """Queries the Reddit API and returns the count of words in
     word_list in the titles of all the hot posts
     of the subreddit"""
+    import requests
+
     sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
                             .format(subreddit),
                             params={"after": after},
