fe48d3c48e3c8de26f179c2bbd2a9bc63195b968
NaomiKleinhans <naomikleinhans56@gmail.com>
2023-08-09 23:49:51 +0200

    update format

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""a function that queries the Reddit API and
-returns the number of subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit is given,
-the function should return 0"""
+"""
+A function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given, the function should return 0
+"""
+
 import requests
 
-
 def number_of_subscribers(subreddit):
-    if subreddit is None or type(subreddit) is not str:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    base_url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': '0x16-api_advanced_project'}
-    r = requests.get(base_url, headers=headers, allow_redirects=False).json()
+    response = requests.get(base_url, headers=headers, allow_redirects=False)
 
-    if r.get('data') is None:
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data.get('data', {}).get('subscribers', 0)
+        return subscribers
+    else:
         return 0
 
-    subscribers = r.get('data').get('subscribers')
-    return subscribers
+subreddit_name = "python"
+subscribers = number_of_subscribers(subreddit_name)
+print(f"Subscribers of /r/{subreddit_name}: {subscribers}")
