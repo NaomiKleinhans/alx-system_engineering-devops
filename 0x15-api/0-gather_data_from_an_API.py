546db75fc0e0d184a0a4f445ea46b43bad94367c
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-07-31 20:58:30 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python3
-
 import requests
 import sys
 
-# Accept Employee ID as a Command-Line Argument
-if len(sys.argv) != 2:
-    print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-    sys.exit(1)
 
-employee_id = sys.argv[1]
+def fetch_employee_tasks(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_url = f"{base_url}users/{employee_id}"
+    todos_url = f"{base_url}todos?userId={employee_id}"
 
-# Fetch Employee Data from the API
-base_url = "https://jsonplaceholder.typicode.com/"
-user_url = f"{base_url}users/{employee_id}"
-todos_url = f"{base_url}todos?userId={employee_id}"
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
 
-user_response = requests.get(user_url)
-todos_response = requests.get(todos_url)
+    if user_response.status_code != 200 or todos_response.status_code != 200:
+        print("Error: Unable to fetch data")
+        sys.exit(1)
 
-if user_response.status_code != 200 or todos_response.status_code != 200:
-    print("Error: Unable to fetch data")
-    sys.exit(1)
+    user = user_response.json()
+    todos = todos_response.json()
 
-user = user_response.json()
-todos = todos_response.json()
+    employee_name = user.get("name")
+    total_tasks = len(todos)
+    completed_tasks = [task for task in todos if task.get("completed")]
+    num_completed_tasks = len(completed_tasks)
 
-# Process and Display the Data
-employee_name = user.get("name")
-total_tasks = len(todos)
-completed_tasks = [task for task in todos if task.get("completed")]
-num_completed_tasks = len(completed_tasks)
+    print(
+        f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t {task.get('title')}")
 
-print(
-    f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
-for task in completed_tasks:
-    print(f"\t {task.get('title')}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+    try:
+        employee_id = int(sys.argv[1])
+        fetch_employee_tasks(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer")
+        sys.exit(1)
