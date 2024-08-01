5c71d9f5c8e0fa5b1aed68126a9ef5dd87c3bbb4
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-01 05:51:03 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""For a given employee ID,returns to-do list info"""
+"""
+For a given employee ID, returns to-do list info
+"""
 
 import requests
 import sys
@@ -10,6 +12,9 @@ if __name__ == "__main__":
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
+
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+
+    for task in completed:
+        print("\t {}".format(task))
